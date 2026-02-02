aa647fb5af93d74ac634837e462f26b50cfd8576
Jarius Miguel Catapang Ballesteros <jrsmglctpngbllstrs@gmail.com>
2026-02-02 12:05:29 +0800

    no migrate

--- start.sh
+++ start.sh
@@ -4,9 +4,8 @@ echo "Waiting for database..."
 
 for i in 1 2 3 4 5
 do
-  php artisan migrate --force && break
-  echo "DB not ready, retrying in 5s..."
-  sleep 5
+  php artisan config:clear
+  php artisan optimize
 done
 
 #!/bin/sh
