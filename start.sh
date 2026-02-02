f52901559f80cdcc0fc5f8e0594ed15c12139863
Jarius Miguel Catapang Ballesteros <jrsmglctpngbllstrs@gmail.com>
2026-02-02 12:58:36 +0800

    start changed

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-# start PHP-FPM only
-php-fpm
+
+echo "Waiting for database..."
+
+for i in 1 2 3 4 5
+do
+  php artisan config:clear
+php artisan config:cache
+
+done
+
+#!/bin/sh
+
+#!/bin/sh
+
+set -e
+
+echo "Starting PHP-FPM..."
+php-fpm &
+
