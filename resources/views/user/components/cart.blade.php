d05b2af285dbf711249095e2aaed20141b0ffae3
Jarius Miguel Catapang Ballesteros <jrsmglctpngbllstrs@gmail.com>
2026-02-04 20:44:26 +0800

    ch file

--- resources/views/user/components/cart.blade.php
+++ resources/views/user/components/cart.blade.php
@@ -47,13 +47,19 @@
   <div class="container">
     <a class="navbar-brand" href="#"><span class="text-warning">Shop</span>Con</a>
     
-     <li class="nav-item">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> 
+        <li class="nav-item">
           <a class="nav-link" href="#home">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#addtocart">Shop</a>
         </li>
-         <li class="nav-item">
+        <li class="nav-item">
           <a class="nav-link" href="{{ route('user.orders') }}">My Orders</a>
         </li>
         <li class="nav-item">
@@ -71,14 +77,7 @@
         <li class="nav-item">
           <a class="nav-link" href="#contact">Contact</a>
         </li>
-                  
       </ul>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">       
-       
     </div>
   </div>
 </nav>
