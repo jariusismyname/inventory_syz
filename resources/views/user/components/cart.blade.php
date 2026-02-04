1e5cb6ad35e4cd87e8bf122d36f471fa55899c5f
Jarius Miguel Catapang Ballesteros <jrsmglctpngbllstrs@gmail.com>
2026-02-04 21:00:23 +0800

    saved changes

--- resources/views/user/components/cart.blade.php
+++ resources/views/user/components/cart.blade.php
@@ -396,7 +396,7 @@
            </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> 
     
 </body>
 </html>
